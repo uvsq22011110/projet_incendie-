0f52e32c651cb2c8d59b6926aa32c6388958d267
uvsq22011110 <uvsq22011110@users.noreply.github.com>
2021-02-09 08:41:54 +0100

    le premier clône du projet

---
+++ incendie.py
@@ -0,0 +1,10 @@
+#########################################
+# groupe MPCI 6
+# OUCHERIF Lounis
+# ZAGBAHI Godi
+# LIGER Arthur
+# MORCOS DOUEIHY Jean-Paul
+# ROBINSON Andry
+#
+# https://github.com/uvsq22011110/projet_incendie-
+#########################################
